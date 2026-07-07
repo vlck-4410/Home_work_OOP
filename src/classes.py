0d9dbfb813df57c1ea2597cd2f5bf7d0fbfa6012
Zarubin Sergey <seregazarubin2003@mail.ru>
2026-07-07 19:13:19 +1000

    some adds

--- src/classes.py
+++ src/classes.py
@@ -4,13 +4,30 @@ from src.read_file import read_file_products
 class Product:
     name = str
     description = str
-    price = float
+    __price = float
     quantity = int
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price < 0:
+            print('Цена не должна быть нулевая или отрицательная')
+            return
+
+        if new_price <= self.__price:
+            user_choice = input(f'Вы уверены, что хотите снизить цену с {self.__price} до {new_price}? (y/n):')
+            if user_choice.lower() != 'y':
+                print('Снижение цены отменено')
+                return
+            self.__price = new_price
 
     @classmethod
     def new_product(cls, product_data: dict, existing_products: list = None):
